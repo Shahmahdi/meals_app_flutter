408fc56830769472abd052fda6bb3d17c16b4b19
Shahmahdi <shahmahdi11@gmail.com>
2024-07-30 18:14:49 +0600

    category and meals screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/categories.dart';
+// import 'package:meals_app/screens/meals.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+var kColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 131, 57, 0),
+);
+
+final theme = ThemeData(
+  colorScheme: kColorScheme,
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,12 +26,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals management app',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Meals app"),
-        ),
-        body: const Text("Home screen"),
-      ),
+      theme: theme,
+      home: const CategoriesScreen(),
+      // home: const MealsScreen(
+      //   title: 'Some meals content',
+      //   meals: dummyMeals,
+      // ),
     );
   }
 }
